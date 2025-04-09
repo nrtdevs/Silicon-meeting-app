31b6571a1e6ed18d2e26996b7cd12c44271eb2d2
SidhdadatriPandey <sidhdadatripandey@gmail.com>
2025-04-09 15:43:12 +0530

    le bhai

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -8,7 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 
 export default function TabTwoScreen() {
-  //test
+  //testf
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
